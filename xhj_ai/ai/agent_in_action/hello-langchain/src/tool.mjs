24bb52717dba3a3141523c13124c3f3bb7ec6ba8
KBr <1943755680@qq.com>
2026-08-24 23:35:22 +0800

    async

--- xhj_ai/ai/agent_in_action/hello-langchain/src/tool.mjs
+++ xhj_ai/ai/agent_in_action/hello-langchain/src/tool.mjs
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import { ChatOpenAI } from '@langchain/openai';
-import { tool } from '@langchain/tools';
+import { tool } from '@langchain/core/tools';
 import { 
     HumanMessage,   // user  role: user
     SystemMessage,  
@@ -60,10 +60,42 @@ const messages = [
           可用工具：
           - read_file：读取文件内容(使用此工具来获取文件内容)
         `),
-        new HumanMessage('请读取 tool.mjs 文件内容并解释代码'),
+        new HumanMessage('请读取 src/tool.mjs 文件内容并解释代码'),
 ];
-
+// 变量 每次调用llm 都会覆盖
+// tools 继续调用工具
+//足够的上下文 可以直接生成了
 let response = await modelWithTools.invoke(messages);
 // console.log(JSON.stringify(response));
 messages.push(response);
 // 多个工具 await read  await write  并发执行？
+// response.tools ,提升性能，有多个任务 Promise.all tool promise 数组
+// tool 执行结果，每个结果带上tool id  ToolMessage 给messages
+// 把整个messages 数组 打包给llm，得到最后的结果
+
+while(response.tool_calls && response.tool_calls.length > 0) {
+    // 调用工具
+    console.log(`\n[检测到 ${response.tool_calls.length} 个工具调用]`);
+    const toolResults = await Promise.all(
+        response.tool_calls.map(async (toolCall) => {
+            // 需要检验 以及准备的逻辑
+            // 找到工具
+            const tool = tools.find(t => t.name === toolCall.name);
+            // 严谨性
+            if (!tool) {
+                return `错误：未找到工具 ${toolCall.name}`;
+            }
+            console.log(`[执行工具] ${toolCall.name}(
+                ${JSON.stringify(toolCall.args)})`);
+                // langchain tool 方法
+                // 容错性处理
+                try {
+                    const result = await tool.invoke(toolCall.args);
+                    return result;
+                } catch(err) {
+                    return `错误：${err.message}`;
+                }
+                const result = await tool.invoke(toolCall.args);
+        })
+    )
+}
--- xhj_ai/ai/agent_in_action/readme2.md
+++ xhj_ai/ai/agent_in_action/readme2.md
@@ -17,3 +17,20 @@ llm + Tool(fs + cli)
 llm 开发框架 比openai(transformer,Generative) 还早诞生
 - llm 有很多家 兼容各家大模型
 @langchain/openai
+
+## Message
+SystemMessage 设置AI是谁，可以干什么，有什么能力，以及一些回答，行为规范等
+HumanMessage 用户的消息
+AIMessage AI的回复消息
+ToolMessage 调用工具的结果返回 Tool id
+
+原生 openai 返回工具调用 additional_kwargs -> tools -> 每个tool
+langchain invoke 原样输出上面的，同时还会细心的准备tools 加到后面
+llm 工程开发的便捷性，可读性 帮助
+
+## AI 工程
+- 工程目录 
+    根目录 package.json node_modules
+- src 开发代码目录 
+    - promise 特性
+    async 函数 就是promise 实例， return resolve 并且return的结果就是
