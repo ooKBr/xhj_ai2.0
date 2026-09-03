fbee2eca38736001e33a06bd35bc9f11c664d95d
KBr <1943755680@qq.com>
2026-09-03 23:55:53 +0800

    rag_splitter 语义切割

--- xhj_ai/ai/agent_in_action/rag_splitter/readme.md
+++ xhj_ai/ai/agent_in_action/rag_splitter/readme.md
@@ -17,3 +17,20 @@ loader 输入是文件，输出是Documents
 2. 分块  一个文件太大，要检索的是一定大小具有一定语义的chunk
 处理模块来自社区 @langchain/community 主要由社区维护，我们都可以写loader
 langchain @langchain/core 官方维护的
+
+- 爬虫 crawl
+  - 从目标url 开始，发送请求，拿到html字符串 axios 简单 
+  - 解析html字符串，提取需要的文本内容（以前需要用到正则） 难度
+  - cheerio 另辟蹊径，前端思维 用 css 选择器，需要的内容
+    cheerio.load(html)   变成document 对象
+    $(css selector).text()  
+
+## AI 时代程序员价值
+- 不再是coding，交给AI
+- vibe coding 问出好的问题（Prompt），提供丰富准确的上下文（Contest）、驾驭（Harness）并部署（FDE）Agent产品，设计长时间稳定运行的Loop，用好AI，快速成为一名AI 架构师。
+
+- 切割的意义
+    保持语义的完整性
+    - separators 按照语义的最基本构造符号 。？！ 进行一个拆分
+    - 按chunkSize 的大小进行切割
+    - 切断了， chunk 的最后一句和下一个chunk 第一句，他们的语义相关性使最大的，但是因为ChunkSize 切开了，语义遗憾，用overlap 用一定的冗余来确保语义的完整性。
--- xhj_ai/ai/agent_in_action/rag_splitter/src/index.mjs
+++ xhj_ai/ai/agent_in_action/rag_splitter/src/index.mjs
@@ -2,15 +2,42 @@ import 'dotenv/config';
 import "cheerio";
 // 从url 加载文档
 import { 
+    // loader 按url 加载
     CheerioWebBaseLoader
 } from "@langchain/community/document_loaders/web/cheerio";
+import {
+    // recursive 递归
+    RecursiveCharacterTextSplitter
+}from '@langchain/textsplitters'
 // 访问网址 并提取文档内容 
 // cheerio 可以传递css 选择器 来提取文档内容 缩小范围
+// 爬取指定内容 + Document标准
 const cheerioLoader = new CheerioWebBaseLoader(
     'https://juejin.cn/post/7680709250218410011',
     {
         selector:'.main-area p'  // p 标签里面的内容一般就是文章段落
     }
 )
+// 大的document 分成小的document，更加精细的去处理语义
+// 按段落划分？ 段落太长？太短？语义分段  
+// 目的是语义精确匹配，重点
+// 按句子来分 。！适合  ，不适合
+// chunk 大小 比如 400 字符
 const documents = await cheerioLoader.load();
-console.log(documents);
+// console.log(documents);
+// 切片
+// 语义排第一位
+// 如果按大小来切割，chunkSize 就够了
+// 为了语义完整，不一定是400 字符
+// 递归 尝试不同的分隔符，找到最优的分隔符，使每个chunk 都有语义，且接近chunkSize
+// 还是有不完美的地方（如最后剩下的不足400 字符），会直接硬切，所以用 chunkOverlap 来补救 重叠切割
+const textSplitter = new RecursiveCharacterTextSplitter({  //recursive 递归  先用句号尝试，再用感叹号和问号，看哪个更加接近400 字符
+    chunkSize: 400,  // 每个chunk 大小  何为chunk？ 一个document 切片，每个切片就是一个chunk
+    separators:["。","！","？",],
+    chunkOverlap: 100
+})
+
+const splitDocuments =
+    await textSplitter.splitDocuments(documents);
+
+console.log(splitDocuments);
