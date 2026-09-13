df339d6cbbb65615744ce035c0e824b765462738
KBr <1943755680@qq.com>
2026-09-13 23:02:29 +0800

    端模型

--- xhj_ai/fe/react/deepseek-r1-webgpu/wbegpu-demo/src/App.tsx
+++ xhj_ai/fe/react/deepseek-r1-webgpu/wbegpu-demo/src/App.tsx
@@ -3,18 +3,109 @@
 // 响应式 
 // 数据绑定
 // 函数封装特性 组件的html，css，js 封装成一个组件
-import { useState } from 'react'
+import { 
+  useState,  // react 函数式思想 hooks，以use 开头
+  useEffect  // 生命周期钩子函数  组件挂载时执行
+} from 'react'
+import './App.css'
 
 function App() {
+  // use 用， status状态  在react中叫 hooks函数
+  // 数据状态驱动界面状态，可以设计状态
+  // 变量/常量 -> 数据（数据绑定 data binding & data driving，
+  // 不需要dom编程了） -> 数据状态（响应式，修改状态，界面会跟着变）
+  // 数据有不同的状态，界面不同的状态 
+  // null 初始值，loading 加载中 ready llm准备好了
+  const [status,setStatus] = useState(null); // 响应式数据状态
+  // 错误对象数据状态
+  const [error,setError] = useState(null); 
+  // 加载信息
+  const [loadingMessage,setLoadingMessage] = useState('');
+  const [progressItems,setProgressItems] = useState([{
+    file: 'model.onnx',
+    progress:0,
+    total:1234567
+  }]);
+  
+  // 浏览器 导航栏 是否支持 WebGPU
+  // 现代浏览器的重要特性
+  // 一个！ 取反   navigator.gpu 不支持的时候 undefined  undefined 再取反结果又变成原来的true|false
+  // !! 再取反，一定可以转成 true | false
+  // 双重否定等于肯定
+   const IS_WEBGPU_AVALABLE = !!navigator.gpu;
+
+   // 组件生命周期， 副作用
+  // 组件挂载后，附带做什么 就叫副作用
+  useEffect(() => {
+    console.log('组件已经挂载完成');
+    // setTimeout(() => {
+    //    setStatus('ready');
+    // }, 2000);
+  }, [])
+
+  console.log('组件函数执行');
+  // js 脚本 数据逻辑交互
   // count 数据状态
   // 修改count  setCount
-  const [count,setCount] = useState(0);  // useState 响应式  相当于vue 里的ref
+  // const [count,setCount] = useState(0);  // useState 响应式  相当于vue 里的ref
   return (
   //  返回html jsx  react的UI表现格式
-  <div className="flex justify-center">
-    Hello World!  {count}
-    <h1 className="text-3xl font-bold underline">你好世界</h1>
-  </div>
+  // <div className="flex justify-center">
+  //   Hello World! {status}
+  //   <h1 className="text-3xl font-bold underline">你好，世界</h1>
+  // </div>
+  // file-direction 主轴 100vh margin x 水平居中对齐
+  // 原子类，组合一下 flex-start flex-end
+  IS_WEBGPU_AVALABLE?(<div className="flex flex-col h-screen mx-auto items-center justify-end text-gray-800 bg-white">
+    <div className="h-full overflow-auto flex justify-center items-center flex-col relative">
+      {/* 
+      1rem = 4 个单位  1 单位 = 4px
+      [] 代表指定样式大小
+      */}
+      <div className="flex flex-col items-center mb-1 max-w-[400px] text-center">
+        {/* 蒸馏的是Qwen */}
+        <h1 className="text-4xl font-bold mb-1">DeepSeek-R1 WebGPU</h1>
+        <h2 className="font-semibold">
+          A next generation reasoning model that runs locally in your brower with WebGPU acceleration.
+        </h2>
+      </div>
+      <div className="flex flex-col items-center px-4">
+        <p className="mx-w-[510px] mb-4">
+          You are about to load 
+            <a
+              href="https://huggingface.co/onnx-community/DeepSeek-R1-Distill-Qwen-1.5B-ONNX"
+              target="_blank"
+              rel="noreferrer"
+              className="font-medium underline"
+            >
+              {/* DeepSeek-R1 的 15 亿参数量蒸馏版，用 Qwen 架构，适合本地轻量推理。
+                蒸馏Qwen  Reasoning 推理模型
+                HuggingFace 抱抱脸 全球最大开源模型社区
+              */}
+              DeepSeek-R1-Distill-Qwen-1.5B
+            </a>
+            , a 1.5B parameter reasoning LLM optimized for in-browser
+              inference. Everything runs entirely in your browser with
+            {
+              // transformers 是 huggingface 推出的js 库，用于加载和推理模型。
+            }
+            <a
+              href="https://huggingface.co/docs/transformers.js"
+              target="_blank"
+              rel="noreferrer"
+              className="underline"
+            >
+              🤗&nbsp;Transformers.js
+            </a>{" "}
+            {/* Open Neural Network Exchange */}
+            and ONNX Runtime Web, meaning no data is sent to a server. Once
+            loaded, it can even be used offline. The source code for the demo
+            is available on{" "}
+        </p> 
+      </div>
+    </div>
+  </div>):(
+    <div>您的浏览器还不支持WebGPU</div>
   )
 }
 
